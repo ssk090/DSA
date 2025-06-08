0e13589813bd3ccf9da6a8f599985bee9d042980
Shivananda Sai <shivanandasai.38@gmail.com>
2025-06-08 13:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 56.1 MB (93.68%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,15 +3,11 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    if (nums.length == 0) {
-        return 0;
-    }
-
     let left = 1;
-    for(let right = 1; right < nums.length; right++) {
-        if(nums[right] !== nums[right - 1]) {
+    for(let right = 1; right < nums.length;right++){
+        if (nums[right] !== nums[right-1]) {
             nums[left] = nums[right];
-            left++;
+            left++
         }
     }
     return left;
