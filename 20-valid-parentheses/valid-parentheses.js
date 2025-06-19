22bf81cfe1809e7c6f02ca1db6f7e4fbd945af84
Shivananda Sai <shivanandasai.38@gmail.com>
2025-06-19 22:41:54 +0530

    Time: 2 ms (74.78%) | Memory: 55.9 MB (49.42%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,19 +3,35 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-    let stack = [];
+    // Solution 1
+    // let stack = [];
+    // for (let i = 0; i < s.length; i++) {
+    //     let char = s[i];
+    //     if (char === "(" || char === "[" || char === "{") {
+    //         stack.push(char);
+    //     } else {
+    //         let prevVal = stack.pop();
+    //         if (prevVal === "(" && char !== ")") return false;
+    //         if (prevVal === "[" && char !== "]") return false;
+    //         if (prevVal === "{" && char !== "}") return false;
+    //         if (prevVal === undefined) return false;
+    //     }
+    // }
+    // return stack.length === 0;
 
-    for (let i = 0; i < s.length; i++) {
-        let char = s[i];
-        if (char === "(" || char === "[" || char === "{") {
+    // Solution 2
+    const stack = [];
+    const map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (let char of s) {
+        if (char === '(' || char === '[' || char === '{') {
             stack.push(char);
         } else {
-            let prevVal = stack.pop();
-
-            if (prevVal === "(" && char !== ")") return false;
-            if (prevVal === "[" && char !== "]") return false;
-            if (prevVal === "{" && char !== "}") return false;
-            if (prevVal === undefined) return false;
+            if (stack.pop() !== map[char]) return false;
         }
     }
 
